b996399d3a64d5f0c7c844c89731025efb92d641
huyldth1908041 <huyldth1908041>
2021-07-06 15:59:48 +0700

    test ssr

--- pages/teachers/[id].js
+++ pages/teachers/[id].js
@@ -12,59 +12,43 @@ export default function Teacher({teacher}) {
    </>
 }
 
-export async function getStaticPaths() {
-   // Return a list of possible value for id
-   //ex:
-   // [
-   //   {
-   //     params: {
-   //       id: 'ssg-ssr'
-   //     }
-   //   },
-   //   {
-   //     params: {
-   //       id: 'pre-rendering'
-   //     }
-   //   }
-   // ]
-   try {
-      const resp = await fetch("http://34.126.65.139:8080/api/v1/teachers");
-      if(!resp.ok) {
-         throw new Error("fetch teachers failed")
-      }
-      const teachers = await resp.json();
-      const paths =  teachers.map(item => {
-         const params = {id: item.id.toString()}
-         return {params}
-      })
-      return {
-         paths,
-         fallback: false
-      }
-   } catch (e) {
-      console.log(e.message)
-      return null;
-   }
-}
-
-export async function getStaticProps({ params }) {
-   // Fetch necessary data for teacher post using params.id
-   const id = params.id;
-   try {
-      const resp = await fetch("http://34.126.65.139:8080/api/v1/teachers/".concat(id));
-      if(resp.ok) {
-         const teacher = await resp.json();
-         return {props: {teacher}}
-      }
-      throw new Error("fetch teacher detail fail")
-   } catch (e) {
-      console.log(e.message)
-      return {props: {teacher: null}};
-   }
-}
-
-
-// export async function getServerSideProps({params}) {
+// export async function getStaticPaths() {
+//    // Return a list of possible value for id
+//    //ex:
+//    // [
+//    //   {
+//    //     params: {
+//    //       id: 'ssg-ssr'
+//    //     }
+//    //   },
+//    //   {
+//    //     params: {
+//    //       id: 'pre-rendering'
+//    //     }
+//    //   }
+//    // ]
+//    try {
+//       const resp = await fetch("http://34.126.65.139:8080/api/v1/teachers");
+//       if(!resp.ok) {
+//          throw new Error("fetch teachers failed")
+//       }
+//       const teachers = await resp.json();
+//       const paths =  teachers.map(item => {
+//          const params = {id: item.id.toString()}
+//          return {params}
+//       })
+//       return {
+//          paths,
+//          fallback: false
+//       }
+//    } catch (e) {
+//       console.log(e.message)
+//       return null;
+//    }
+// }
+//
+// export async function getStaticProps({ params }) {
+//    // Fetch necessary data for teacher post using params.id
 //    const id = params.id;
 //    try {
 //       const resp = await fetch("http://34.126.65.139:8080/api/v1/teachers/".concat(id));
@@ -78,3 +62,19 @@ export async function getStaticProps({ params }) {
 //       return {props: {teacher: null}};
 //    }
 // }
+
+
+export async function getServerSideProps({params}) {
+   const id = params.id;
+   try {
+      const resp = await fetch("http://34.126.65.139:8080/api/v1/teachers/".concat(id));
+      if(resp.ok) {
+         const teacher = await resp.json();
+         return {props: {teacher}}
+      }
+      throw new Error("fetch teacher detail fail")
+   } catch (e) {
+      console.log(e.message)
+      return {props: {teacher: null}};
+   }
+}
